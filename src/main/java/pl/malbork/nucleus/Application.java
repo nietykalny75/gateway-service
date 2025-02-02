aef52166a8b1ee9a01e9246d9268707bba31113e
nietykalny75 <tomasz.sowinski@nucleus.malbork.pl>
2025-02-02 02:26:11 +0100

    Upgrade artifacts versions

--- src/main/java/pl/malbork/nucleus/Application.java
+++ src/main/java/pl/malbork/nucleus/Application.java
@@ -3,9 +3,11 @@ package pl.malbork.nucleus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableWebFluxSecurity
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
